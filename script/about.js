8950e41ab57f805e88b7479ec7cb9195d2be4870
Thomas Bella <github@unterhaltungsbox.com>
2015-02-23 14:44:01 +0100

    Get changelog from GitHub

--- script/about.js
+++ script/about.js
@@ -41,4 +41,53 @@ $(document).ready(function()
 
 
 	$("title").html($("title").text() + " &bull; uDomainFlag");
+
+	$(".spoiler .title").click(function (){
+		$(".spoiler .content").slideToggle("fast");
+	});
+
+	// Get releases from github
+	$.get("https://api.github.com/repos/Thomas2500/uDomainFlag/releases", function (data)
+	{
+		$.each(data, function (i, v)
+		{
+			var date = v.published_at.split('T');
+			date = date[0].split('-');
+
+			var html = "<div class=\"item\">";
+			html += "<div class=\"title\"><span class=\"v\"><a href=\""+v.html_url+"\">"+v.tag_name+"</a></span><br />"+date[2]+"."+date[1]+"."+date[0]+"</div>";
+			html += "<div class=\"option\">";
+
+			var lines = v.body.match(/[^\r\n]+/g);
+			var ul = 0;
+
+			$.each(lines, function (i2, l)
+			{
+				// Check if unordert list is used
+				if (l.substring(0, 2) === "- ")
+				{
+					if (ul == 0)
+					{
+						html += "<ul>";
+						ul = 1;
+					}
+
+					html += "<li>" + l.replace("- ", "").trim() + "</li>";
+				}
+				else if (ul == 1)
+				{
+					html += "</ul>";
+					ul = 0;
+				}
+			});
+			if (ul == 1)
+				html += "</ul>";
+
+			html += "</div>";
+			html += "<div class=\"clear line\"></div>";
+			html += "</div>";
+
+			$(".autochangelog").append(html);
+		});
+	}, "json");
 });
